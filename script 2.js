8ef5de42be4e07cda2da18cca2f4e61d83edde4f
cresentokello32 <166908793+cresentokello32@users.noreply.github.com>
2024-04-13 12:18:08 +0300

    Add files via upload

---
+++ script 2.js
@@ -0,0 +1,9 @@
+
+document.getElementById('contact-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    showContent('contact');
+});
+function showContent(page) {
+document.getElementById('contact-content').style.display = 'none';
+    document.getElementById(page + '-content').style.display = 'block';
+}
